64a87a8ce8e5955e948953fdbca0f0b8750831f8
saikrishna7004 <74298585+saikrishna7004@users.noreply.github.com>
2021-05-26 07:56:52 +0530

    Update app.py

--- app.py
+++ app.py
@@ -49,5 +49,12 @@ def delete(sno):
     db.session.commit()
     return redirect("/")
 
+@app.before_request
+def before_request():
+    if not request.is_secure:
+        url = request.url.replace('http://', 'https://', 1)
+        code = 301
+        return redirect(url, code=code)
+
 if __name__ == '__main__':
     app.run()
